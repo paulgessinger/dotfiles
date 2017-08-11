c6d597aec749170b8771f20b7e56e5eb4a5f858f
Paul Gessinger <hello@paulgessinger.com>
2017-08-11 14:13:34 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+mv ~/.bashrc ~/.bashrc.orig 2>/dev/null
+mv ~/.vimrc ~/.vimrc.orig 2>/dev/null
+mv ~/.gitconfig ~/.gitconfig.orig 2>/dev/null
+mv ~/.tmux.conf ~/.tmux.conf.orig 2>/dev/null
+mv ~/.zshrc ~/.zshrc.orig 2>/dev/null
+mv ~/.bash_profile ~/.bash_profile.orig 2>/dev/null
+
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 mkdir ~/.vim
