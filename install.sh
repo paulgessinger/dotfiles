158e4b65e9cda1eb616d5ba334627391509ccfa0
Paul Gessinger <hello@paulgessinger.com>
2018-10-15 16:30:01 +0200

    Fix git completion

--- install.sh
+++ install.sh
@@ -18,4 +18,5 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
